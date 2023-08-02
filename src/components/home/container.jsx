8b09e0ee0c537306eb6c5cfaf68bb3fdf71b4784
cloby99 <2020cs020@stu.ucsc.cmb.ac.lk>
2023-08-02 13:58:08 +0530

    Created the home page

--- src/components/home/container.jsx
+++ src/components/home/container.jsx
@@ -5,6 +5,28 @@ function container() {
   return (
     <div className='home__body__container'>
         <img src={HOMEIMG} alt="" />
+
+        <div className='home__body-right'>
+            <div className="home__body-rightTop">
+              <h2>
+                Find your new best friend Today!
+              </h2>
+              <div>
+                <p>
+                  Explore our gallery of cute and  
+                </p>
+                <p>
+                  cuddly stray animals who are ready
+                </p>
+                <p>
+                  to join your family.
+                </p>
+              </div>
+            </div>
+            
+            <button>Get Started </button>
+            
+        </div>
     </div>
   )
 }
